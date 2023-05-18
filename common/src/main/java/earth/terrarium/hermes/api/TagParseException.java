17b507ad4afe8c24641e0c936591c5de60d58b27
ThatGravyBoat <thatgravyboat@gmail.com>
2023-05-18 05:06:44 -0230

    Switch to proper document parser

--- common/src/main/java/earth/terrarium/hermes/api/TagParseException.java
+++ common/src/main/java/earth/terrarium/hermes/api/TagParseException.java
@@ -2,6 +2,10 @@ package earth.terrarium.hermes.api;
 
 public class TagParseException extends RuntimeException {
 
+    public TagParseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
     public TagParseException(String message) {
         super(message);
     }
--- common/src/main/java/earth/terrarium/hermes/api/TagProvider.java
+++ common/src/main/java/earth/terrarium/hermes/api/TagProvider.java
@@ -1,14 +1,22 @@
 package earth.terrarium.hermes.api;
 
-import org.jetbrains.annotations.NotNull;
+import org.apache.commons.lang3.StringUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
 
-import java.util.*;
-import java.util.regex.Pattern;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class TagProvider {
-
-    private static final Pattern LESS_THAN_REPLACE = Pattern.compile("([^\\\\])\\\\<|^\\\\<");
-    private static final Pattern GREATER_THAN_REPLACE = Pattern.compile("([^\\\\])\\\\>|^\\\\>");
 
     private final Map<String, TagElementSerializer> serializers = new HashMap<>();
 
@@ -17,130 +25,62 @@ public class TagProvider {
     }
 
     public List<TagElement> parse(String text) {
-        Stack<TagElement> stack = new Stack<>();
-        stack.push(new Root(new ArrayList<>()));
-        StringBuilder content = new StringBuilder();
-
-        int index = 0;
-        while (index < text.length()) {
-            int openIndex = indexOfUnescaped('<', text, index);
-            if (openIndex == -1) break;
-            int closeIndex = indexOfUnescaped('>', text, openIndex);
-            if (closeIndex == -1) break;
-
-            String tag = text.substring(openIndex + 1, closeIndex);
-            String tagName = tag.split(" ")[0];
-
-            if (tagName.charAt(0) == '/') {
-                content.append(text, index, openIndex);
-                var pop = stack.pop();
-                if (pop.getChildren().size() > 0 && isContentNotEmpty(content)) {
-                    throw new TagParseException(
-                        "Cannot have content and children. Content: " + content
-                    );
-                }
-                if (isContentNotEmpty(content)) {
-                    pop.setContent(formatContent(content.toString()));
-                    content = new StringBuilder();
-                }
-                stack.peek().addChild(pop);
-                index = closeIndex + 1;
-                continue;
-            } else {
-                content.append(text, index, openIndex);
-                if (isContentNotEmpty(content)) {
-                    throw new TagParseException(
-                        "Cannot have content and children. Content: " + content
-                    );
-                }
-                boolean isSelfClosing = tag.endsWith("/");
-                if (isSelfClosing) {
-                    tag = tag.substring(0, tag.length() - 1);
-                }
-                Map<String, String> parameters = parseParameters(tag, tagName);
-                if (!this.serializers.containsKey(tagName)) {
-                    throw new TagParseException(
-                        "Unknown tag: " + tagName
-                    );
-                }
-                stack.push(this.serializers.get(tagName).deserialize(parameters));
-                if (isSelfClosing) {
-                    var pop = stack.pop();
-                    stack.peek().addChild(pop);
-                }
-            }
-
-            index = closeIndex + 1;
+        try {
+            InputStream stream = new ByteArrayInputStream(("<root>" + text + "</root>").getBytes());
+            Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(stream);
+            Node root = document.getChildNodes().item(0);
+            root.normalize();
+            return nodeToElements(root);
+        }catch (ParserConfigurationException | SAXException | IOException e) {
+            throw new TagParseException("Failed to parse tag text", e);
         }
-
-        return stack.peek().getChildren();
     }
 
-    private static String formatContent(String content) {
-        content = LESS_THAN_REPLACE.matcher(content).replaceAll(result -> result.groupCount() > 0 ? result.group(1) + "<" : "<");
-        content = GREATER_THAN_REPLACE.matcher(content).replaceAll(result -> result.groupCount() > 0 ? result.group(1) + ">" : ">");
-        return content.trim();
-    }
-
-    private static int indexOfUnescaped(char c, String text, int index) {
-        while (index < text.length()) {
-            if (text.charAt(index) == c) {
-                if (index == 0 || text.charAt(index - 1) != '\\') {
-                    return index;
+    private List<TagElement> nodeToElements(Node node) {
+        List<TagElement> elements = new ArrayList<>();
+        for (int i = 0; i < node.getChildNodes().getLength(); i++) {
+            Node child = node.getChildNodes().item(i);
+            if (child.getNodeType() == Node.ELEMENT_NODE) {
+                TagElementSerializer serializer = serializers.get(child.getNodeName());
+                if (serializer != null) {
+                    TagElement element = serializer.deserialize(mapAttributes(child.getAttributes()));
+                    if (element != null) {
+                        elements.add(element);
+                        int elementNodes = getLengthOfType(child, Node.ELEMENT_NODE);
+                        if (elementNodes > 0) {
+                            nodeToElements(child).forEach(element::addChild);
+                        } else {
+                            String text = child.getTextContent();
+                            if (StringUtils.isNotBlank(text)) {
+                                element.setContent(text);
+                            }
+                        }
+                    }
+                } else {
+                    throw new TagParseException("Unknown tag: " + child.getNodeName());
                 }
             }
-            index++;
         }
-        return -1;
+        return elements;
     }
 
-    private static boolean isContentNotEmpty(StringBuilder builder) {
-        for (int i = 0; i < builder.length(); i++) {
-            if (!Character.isWhitespace(builder.charAt(i))) {
-                return true;
+    private int getLengthOfType(Node node, short type) {
+        int length = 0;
+        for (int i = 0; i < node.getChildNodes().getLength(); i++) {
+            Node child = node.getChildNodes().item(i);
+            if (child.getNodeType() == type) {
+                length += child.getTextContent().length();
             }
         }
-        return false;
+        return length;
     }
 
-    private static Map<String, String> parseParameters(String content, String tag) {
-        String parametersText = content.substring(tag.length()).trim();
-        Map<String, String> parameters = new HashMap<>();
-        String key = null;
-        int index = 0;
-        while (index < parametersText.length()) {
-            if (key == null) {
-                if (parametersText.charAt(index) == ' ') {
-                    index++;
-                    continue;
-                }
-                int equalsIndex = parametersText.indexOf('=', index);
-                if (equalsIndex == -1) break;
-                key = parametersText.substring(index, equalsIndex);
-                index = equalsIndex + 1;
-                continue;
-            }
-            if (parametersText.charAt(index) == '"') {
-                int endIndex = parametersText.indexOf('"', index + 1);
-                if (endIndex == -1) break;
-                parameters.put(key, parametersText.substring(index + 1, endIndex));
-                key = null;
-                index = endIndex + 1;
-            }
+    private Map<String, String> mapAttributes(NamedNodeMap map) {
+        Map<String, String> attributes = new HashMap<>();
+        for (int i = 0; i < map.getLength(); i++) {
+            Node attribute = map.item(i);
+            attributes.put(attribute.getNodeName(), attribute.getNodeValue());
         }
-        return parameters;
-    }
-
-    private record Root(List<TagElement> children) implements TagElement {
-
-        @Override
-        public void addChild(TagElement element) {
-            children.add(element);
-        }
-
-        @Override
-        public @NotNull List<TagElement> getChildren() {
-            return children;
-        }
+        return attributes;
     }
 }
