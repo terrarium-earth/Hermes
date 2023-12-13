0d00f6a6a0d6b041fed66ce92ef0049375e101b1
Dietrich Friday <friday.dietrich@gmail.com>
2023-12-13 21:06:13 +0900

    Correctly stye "scale" class variable

--- common/src/main/java/earth/terrarium/hermes/api/defaults/HeadingTagElement.java
+++ common/src/main/java/earth/terrarium/hermes/api/defaults/HeadingTagElement.java
@@ -11,21 +11,22 @@ import java.util.Map;
 
 public abstract class HeadingTagElement extends TextTagElement {
 
-    public final int SCALE;
+    public final int scale;
+
     public HeadingTagElement(Map<String, String> parameters, int scale) {
         super(parameters);
-        this.SCALE = scale;
+        this.scale = scale;
     }
 
     @Override
     public void render(Theme theme, GuiGraphics graphics, int x, int y, int width, int mouseX, int mouseY, boolean hovered, float partialTicks) {
         try (var ignored = new CloseablePoseStack(graphics)) {
-            graphics.pose().scale(SCALE, SCALE, SCALE);
-            float translationFactor = 1 + (1f / (SCALE - 1));
+            graphics.pose().scale(scale, scale, scale);
+            float translationFactor = 1 + (1f / (scale - 1));
             graphics.pose().translate(-x / translationFactor, -y / translationFactor, 0);
             Component text = Component.nullToEmpty(this.content).copy().setStyle(this.getStyle());
             int height = 0;
-            for (FormattedCharSequence sequence : Minecraft.getInstance().font.split(text, (width - 10) / SCALE)) {
+            for (FormattedCharSequence sequence : Minecraft.getInstance().font.split(text, (width - 10) / scale)) {
                 theme.drawText(
                         graphics,
                         sequence,
@@ -39,13 +40,13 @@ public abstract class HeadingTagElement extends TextTagElement {
 
     @Override
     public int getHeight(int width) {
-        int lines = Minecraft.getInstance().font.split(Component.nullToEmpty(this.content), (width - 10) / SCALE).size();
-        return lines * (Minecraft.getInstance().font.lineHeight + 1) * SCALE;
+        int lines = Minecraft.getInstance().font.split(Component.nullToEmpty(this.content), (width - 10) / scale).size();
+        return lines * (Minecraft.getInstance().font.lineHeight + 1) * scale;
     }
 
     @Override
     public int getXOffset(int x, int width, FormattedCharSequence text) {
-        int textWidth = SCALE * Minecraft.getInstance().font.width(text);
-        return Boolean.TRUE.equals(this.centered) ? x + (int) ((((width - textWidth) / 2f) / SCALE) + 0.5f) : x;
+        int textWidth = scale * Minecraft.getInstance().font.width(text);
+        return Boolean.TRUE.equals(this.centered) ? x + (int) ((((width - textWidth) / 2f) / scale) + 0.5f) : x;
     }
 }
