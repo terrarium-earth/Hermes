4e5f5353e7b2796edf920514d205a1da30750587
Dietrich Friday <friday.dietrich@gmail.com>
2023-12-14 16:13:38 +0900

    <item> tag can now left, center, and right align
    About the same in implemenation details as for <image> tag.

--- common/src/main/java/earth/terrarium/hermes/api/defaults/ItemTagElement.java
+++ common/src/main/java/earth/terrarium/hermes/api/defaults/ItemTagElement.java
@@ -1,6 +1,7 @@
 package earth.terrarium.hermes.api.defaults;
 
 import com.teamresourceful.resourcefullib.client.CloseablePoseStack;
+import earth.terrarium.hermes.api.Alignable;
 import earth.terrarium.hermes.api.TagElement;
 import earth.terrarium.hermes.api.themes.Theme;
 import earth.terrarium.hermes.utils.ElementParsingUtils;
@@ -13,10 +14,11 @@ import net.minecraft.world.item.Items;
 
 import java.util.Map;
 
-public class ItemTagElement implements TagElement {
+public class ItemTagElement implements TagElement, Alignable {
 
     protected final ItemStack output;
     protected final float scale;
+    protected final Alignment align;
 
     public ItemTagElement(Map<String, String> parameters) {
         Item item = ElementParsingUtils.parseItem(parameters, "id", Items.AIR);
@@ -25,12 +27,19 @@ public class ItemTagElement implements TagElement {
         if (tag != null) stack.setTag(tag);
         this.output = stack;
         this.scale = ElementParsingUtils.parseFloat(parameters, "scale", 1.0F);
+        this.align = ElementParsingUtils.parseAlignment(parameters, "align", Alignment.CENTER);
     }
 
     @Override
     public void render(Theme theme, GuiGraphics graphics, int x, int y, int width, int mouseX, int mouseY, boolean hovered, float partialTicks) {
         try (var pose = new CloseablePoseStack(graphics)) {
-            int actualX = x + (int) ((width / 2f) - (8 * scale));
+
+            int actualX = switch (align) {
+                case LEFT -> x;
+                case CENTER -> x + (int) (((width / 2f) - (8 * scale)) + 0.5f);
+                case RIGHT -> x + (int) (width - (16 * scale) + 0.5f);
+            };
+
             pose.translate(actualX, y + 1, 0);
             pose.scale(scale, scale, 1.0F);
             graphics.renderFakeItem(output, 0, 0);
