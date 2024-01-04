e62768361664264f04be0b0699355222baef960b
Dietrich Friday <friday.dietrich@gmail.com>
2024-01-04 12:27:52 +0900

    EntityTagElement extends FillAndBorderElement
    - Explanatory block comment added
    - Renames and changes motivated by keeping clear:
    	- renderEntityInInventoryFollowsMouse()'s pov (rendering from bottom center of the bounding box)
    	- How this element fits in the layout of elements
    
    - Obvious changes that come with extending/using FillAndBorderElement:
    	- Calling super(parameters)
    	- Using `verticalSpacing` in render(), as renderY, and in getHeight()
    
    - Added a `vshift` attribute for the tag
    	- Allows the entity to be rendered higher (or lower), so they can coincide better with their layout box.
    		- Some vanilla entities (squid for one example) render mostly _below_ their bounding box.
    		- Possibly modded entities will also need this.

--- common/src/main/java/earth/terrarium/hermes/api/defaults/EntityTagElement.java
+++ common/src/main/java/earth/terrarium/hermes/api/defaults/EntityTagElement.java
@@ -14,28 +14,40 @@ import net.minecraft.world.entity.LivingEntity;
 
 import java.util.Map;
 
-public class EntityTagElement implements TagElement {
+public class EntityTagElement extends FillAndBorderElement implements TagElement {
 
     private final EntityType<?> type;
     private final CompoundTag tag;
     private final static int BLOCK_HEIGHT = 24;
     private final float scale;
-    private float entityBlocksHigh;
+    private float layoutBlocksHigh;
+    private final float vShift;
     private final Alignment align;
-    private float entityBlocksWide;
+    private float layoutBlocksWide;
     private Entity entity;
 
     public EntityTagElement(Map<String, String> parameters) {
+        super(parameters);
         this.type = ElementParsingUtils.parseEntityType(parameters, "type", null);
         this.tag = ElementParsingUtils.parseTag(parameters, "tag", null);
-        this.scale = ElementParsingUtils.parseFloat(parameters, "scale", 1.0f);
-        this.entityBlocksHigh = ElementParsingUtils.parseFloat(parameters, "height", 0.0f);
         this.align = ElementParsingUtils.parseAlignment(parameters, "align", Alignment.MIDDLE);
-        this.entityBlocksWide = ElementParsingUtils.parseFloat(parameters, "width", 0.0f);
+        this.scale = ElementParsingUtils.parseFloat(parameters, "scale", 1.0f);
+        this.vShift = ElementParsingUtils.parseFloat(parameters, "vshift", 0.0f);
+        this.layoutBlocksHigh = Math.abs(ElementParsingUtils.parseFloat(parameters, "height", 0.0f));
+        this.layoutBlocksWide = Math.abs(ElementParsingUtils.parseFloat(parameters, "width", 0.0f));
     }
 
     @Override
     public void render(Theme theme, GuiGraphics graphics, int x, int y, int width, int mouseX, int mouseY, boolean hovered, float partialTicks) {
+        /*
+        Note on layout and rendering of living entities:
+        Only the bounding box's dimensions are straight-forwardly available; the render call refers only to it.
+        However, several mobs' models visually exceed their bounding box. The horse and squid are two examples.
+        The 'width' and 'height' tag attributes affect the conceptual box that is implied by the layout.
+        The 'vshift' attribute shifts the render vertically in relation to actual bounding box; + is up, - is down.
+        The fill-able background area should correspond to the implied-by-layout box.
+        */
+
         if (this.type != null) {
             if (entity == null && Minecraft.getInstance().level != null) {
                 entity = this.type.create(Minecraft.getInstance().level);
@@ -46,28 +58,36 @@ public class EntityTagElement implements TagElement {
 
             if (entity instanceof LivingEntity living) {
 
-                if (entityBlocksHigh == 0.0f) {
-                    entityBlocksHigh = living.getBbHeight();
+                if (layoutBlocksHigh == 0.0f) {
+                    layoutBlocksHigh = living.getBbHeight();
                 }
-                if (entityBlocksWide == 0.0f) {
-                    entityBlocksWide = living.getBbWidth();
+                if (layoutBlocksWide == 0.0f) {
+                    layoutBlocksWide = living.getBbWidth();
                 }
 
                 int blockScale = Math.round(scale * BLOCK_HEIGHT);
-                float entityHeight = blockScale * entityBlocksHigh;
-                float entityWidth = blockScale * entityBlocksWide;
+                float layoutHeight = blockScale * layoutBlocksHigh;
+                float layoutWidth = blockScale * layoutBlocksWide;
 
-                int offsetX = Alignment.getOffsetCenterDrawnElement(width, entityWidth, align);
-                int offsetY = Math.round(entityHeight);
-                int eyeOffset = Math.round(entityHeight - (living.getEyeHeight() * blockScale));
+                int layoutX = x + Alignment.getOffset(width, layoutWidth, align);
+                int renderX = Math.round(layoutX + (layoutWidth / 2f));
 
-                InventoryScreen.renderEntityInInventoryFollowsMouse(graphics, x + offsetX, y + offsetY + 1, blockScale, x + offsetX - mouseX, y + eyeOffset - mouseY, living);
+                int layoutY = y + verticalSpacing;
+                int renderY = Math.round(layoutY + layoutHeight - (vShift * blockScale));
+
+                int eyeY = layoutY + Math.round(layoutHeight - (living.getEyeHeight() * blockScale));
+                int lookX = renderX - mouseX;
+                int lookY = eyeY - mouseY;
+
+                drawBackground(graphics, layoutX, layoutY, layoutWidth, layoutHeight);
+                InventoryScreen.renderEntityInInventoryFollowsMouse(graphics, renderX, renderY, blockScale, lookX, lookY, living);
             }
         }
     }
 
     @Override
     public int getHeight(int width) {
-        return Math.round((entityBlocksHigh * scale * BLOCK_HEIGHT) + 2);
+        return Math.round((layoutBlocksHigh * scale * BLOCK_HEIGHT)) + (2 * verticalSpacing); // (layoutHeight) + verticalPadding
     }
+
 }
