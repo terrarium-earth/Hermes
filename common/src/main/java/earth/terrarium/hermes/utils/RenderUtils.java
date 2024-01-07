9adf2fa1c59e982c922f8a9a8151eceff64055d1
Dietrich Friday <friday.dietrich@gmail.com>
2024-01-08 06:19:01 +0900

    'width', 'height' arg names for renderOutline()

--- common/src/main/java/earth/terrarium/hermes/utils/RenderUtils.java
+++ common/src/main/java/earth/terrarium/hermes/utils/RenderUtils.java
@@ -4,13 +4,13 @@ import net.minecraft.client.gui.GuiGraphics;
 
 public final class RenderUtils {
 
-    public static void renderOutline(GuiGraphics graphics, int x, int y, int outerWidth, int outerHeight, int thickness, int color) {
+    public static void renderOutline(GuiGraphics graphics, int x, int y, int width, int height, int thickness, int color) {
 
         // Like graphics.renderOutline(), but also takes a thickness argument.
         // Outline segments are drawn to avoid over-lap in case color's alpha < 0xFF
 
-        int x0 = x; int x1 = x + outerWidth;
-        int y0 = y; int y1 = y + outerHeight;
+        int x0 = x; int x1 = x + width;
+        int y0 = y; int y1 = y + height;
         graphics.fill(x0, y0, x1, y0 + thickness, color); // top
         graphics.fill(x0, y1, x1, y1 - thickness, color); // bottom
 
