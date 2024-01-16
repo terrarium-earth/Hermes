5918fb9735b3909244e97bcc4b080a52c61463e6
Dietrich Friday <friday.dietrich@gmail.com>
2024-01-16 22:42:30 +0900

    Huh. IntelliJ mixup(?) with auto adding a needed import

--- common/src/main/java/earth/terrarium/hermes/utils/ElementParsingUtils.java
+++ common/src/main/java/earth/terrarium/hermes/utils/ElementParsingUtils.java
@@ -8,6 +8,7 @@ import net.minecraft.nbt.TagParser;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
+import org.jetbrains.annotations.Nullable;
 
 import java.net.URL;
 import java.util.Map;
